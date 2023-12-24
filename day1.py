7e9c889983a3700ef07e7f7bf145fc119c9a90db
pantol <forkindstuff321@gmail.com>
2023-12-24 10:09:07 +0100

    first anwser

--- day1.py
+++ day1.py
@@ -1,2 +1,17 @@
-#!/usr/bin/env python3 
-print("hello world")
+#!/usr/bin/env python3
+
+with open('ex1_notes.txt', 'r') as file:
+    open_file = file.readlines()
+    total_number = [] 
+    for i in open_file:
+        number = []
+        for j in i:
+            if j.isdigit():
+                number.append(j)
+        total_number.append(int(number[0] + number[-1]))
+    print(sum(total_number))
+    
+#        total_number.append(number[0] + number[-1])
+#        for result in total_number:
+#            print(result)
+#
