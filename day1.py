6abd8bdb1d08fb6af68e64ac1faf312532e3a3e1
pantol <forkindstuff321@gmail.com>
2023-12-26 16:20:01 +0100

    fixed

--- day1.py
+++ day1.py
@@ -1,17 +1,48 @@
 #!/usr/bin/env python3
 
-with open('ex1_notes.txt', 'r') as file:
-    open_file = file.readlines()
-    total_number = [] 
-    for i in open_file:
-        number = []
-        for j in i:
-            if j.isdigit():
-                number.append(j)
-        total_number.append(int(number[0] + number[-1]))
-    print(sum(total_number))
+with open('ex_notes.txt', 'r') as file:
+    opened_file = file.readlines()
+    sum_nubmer1 = 0
+    sum_nubmer2 = 0
+    num = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+
+    def change(i):
+        for n,nn in enumerate(num):
+            i = i.replace(nn, f"{nn}{n}{nn}")
+            #print(n,nn)
+            
+        return i
+
+    for i in opened_file:
+        total_number = [j for j in i if j.isnumeric()]
+        #print(total_number)
+        sum_nubmer1 += int(total_number[0] + total_number[-1])
     
-#        total_number.append(number[0] + number[-1])
-#        for result in total_number:
-#            print(result)
-#
+    for i in opened_file:
+        total_number = [j for j in change(i) if j.isnumeric()]
+        #print(total_number)
+        sum_nubmer2 += int(total_number[0] + total_number[-1])
+
+
+    print(sum_nubmer1, sum_nubmer2)
+
+
+another_example = """
+digits_num = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+def tmp(path=None):
+    path = open(f'{path}','r').readlines()
+    sum_nubmer = 0
+    for line in path: 
+        total_number = [j for j in change(line) if j.isnumeric()]
+        sum_nubmer += int(total_number[0] + total_number[-1])
+    return sum_nubmer 
+
+
+def change(line):
+    for num,name in enumerate(digits_num):
+        line = line.replace(name, f"{name}{num}{name}")
+    return line 
+
+path = 'ex_notes.txt'
+tmp(path)
+"""
